6cf7a8ddd1ee2d3bd7029c144b64f28d05053f9e
Vitalii Lytvynov <vyacheslavxxxxx@gmail.com>
2023-07-09 21:16:34 +0300

    buble sorting

--- src/main/java/lesson5/BubbleSorting.java
+++ src/main/java/lesson5/BubbleSorting.java
@@ -2,17 +2,22 @@ package lesson5;
 
 public class BubbleSorting {
     public static void main(String[] args) {
-        int[] n = {1, -2, 3, -4, 5, 12};
+        int[] n = {1, -3, 4, -5, -13, 6, 11};
 
-
+        int count = 0;
         for (int i = 0; i < n.length - 1; i++) {
-            if (n[i] > n[i + 1]) {
-                int temp = n[i]; // 1
-                n[i] = n[i + 1]; // {-2, -2}
-                n[i + 1] = temp;// {-2, 1}
+            for (int j = 0; j < n.length - 1 - i; j++) {
+                if (n[j] > n[j + 1]) {
+                    int temp = n[j];
+                    n[j] = n[j + 1];
+                    n[j + 1] = temp;
+                }
+                count++;
             }
         }
 
+        System.out.println(count);
+        System.out.println();
         for (int number : n) {
             System.out.print(number + " ");
         }
