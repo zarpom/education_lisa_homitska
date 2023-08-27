7db734ca9507070596c40ec450b399114ab04cc0
Vitalii Lytvynov <vyacheslavxxxxx@gmail.com>
2023-08-27 19:56:59 +0300

    lesson 14

---
+++ src/main/java/lesson14/Main.java
@@ -0,0 +1,70 @@
+package lesson14;
+
+public class Main {
+    public static void main(String[] args) {
+//        String status = "";
+//        double money = -1;
+//
+//        status = money < 0 ? "боржник" : "можна дати в борг";
+//
+//        int age = 100;
+//        int karma = age > 60 ? -1 : 10;
+//        System.out.println(karma);
+
+
+//        if (money < 0) {
+//            status = "боржник";
+//        } else {
+//            status = "можна дати в борг";
+//        }
+//        System.out.println(status);
+
+//        int number = 123;
+//        int sumOfNumber = 0;
+//        int resultNumber = 0;
+//        while (number != 0) {
+//            sumOfNumber += number % 10;
+//            number /= 10;
+//        }
+//
+//        resultNumber = (sumOfNumber > 7 && sumOfNumber % 2 == 0) ? (sumOfNumber / 2) : (sumOfNumber * sumOfNumber);
+//        System.out.println(resultNumber);
+
+
+        String status = "сплю";
+
+//        if (status == "сплю") {
+//            System.out.println("Герой спить");
+//        } else if (status == "гуляю") {
+//            System.out.println("герой гуляє");
+//        } else if (status == "готує") {
+//            System.out.println("Герой готує");
+//        } else {
+//            System.out.println("Ми не знаємо де герой");
+//        }
+//        for (int i = 0; i < 10; i++) {
+//            if (i == 5) {
+//                break;
+//            }
+//            System.out.println(i);
+//
+//        }
+
+//        switch (status) {
+//            case "сплю":
+//                System.out.println("Герой спить");
+//                break;
+//            case "гуляю":
+//                System.out.println("герой гуляє");
+//                break;
+//            case "готує":
+//                break;
+//            default:
+//                System.out.println("Ми не знаємо де герой");
+//        }
+
+
+
+    }
+
+}
